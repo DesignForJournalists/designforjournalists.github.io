159bc26ccec2d388490cc4ef82a48481937923f2
dylbop <dylangreif@Dylans-MacBook-Pro.local>
2016-05-12 11:08:43 -0400

    adding student work

---
+++ final/students/claire-1/Menu Page/js/script.js
@@ -0,0 +1,16 @@
+// hide and show nav block title
+function navAppear( headerId ) { 
+    $( headerId ).addClass('hide');
+}
+function navHide( headerId ) {  
+    $( headerId ).removeClass('hide');
+}
+
+// hide and show nav block title
+function listAppear( navId ) {  
+    $( navId ).removeClass('hide');
+}
+function listHide( navId ) {  
+    $( navId ).addClass('hide');
+}
+

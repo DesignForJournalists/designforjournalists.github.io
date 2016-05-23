041c1321148ee80535cb9fb1b2c797eff72a42a8
dylbop <dylangreif@Dylans-MacBook-Pro.local>
2016-05-23 17:49:25 -0400

    adding paul tough staging

---
+++ tough/web/js/strings.js
@@ -0,0 +1,216 @@
+$(function() {
+   
+    $( ".caption-gilbert" )
+        .append( "Elizabeth Spiegel teaching at I.S. 318. Credit: Bruce Gilbert" );
+   
+    $( ".caption-low-income" )
+        .append( "A Growing Number of  American Public-School Students Are “Low-Income”" );
+   
+    $( ".caption-social-mob" )
+        .append( "Low-Income Children Rarely Experience Social Mobility — Unless They Have a College Degree" );
+    
+    $( ".caption-cont-spread" )
+        .append( "Video of “Ready To Be Counted: Incorporating Noncognitive Skills Into Education Policy,” a panel discussion at the Brookings Institution in Washington, D.C., on March 31, 2015." );
+   
+    $( ".caption-wide-ack" )
+        .append( "Video of James Heckman Discussing “The Hard Facts Behind Soft Skills”" );
+   
+    $( ".caption-spiegel" )
+        .append( "Video of Paul Tough discussing Elizabeth Spiegel on “The State of Education in Nebraska” on Nebraska Public Television, April 2014" );
+   
+    $( ".caption-both-phys" )
+        .append( "“Toxic Stress Derails Healthy Development,” a video from the Center on the Developing Child at Harvard University" );
+   
+    $( ".caption-affect-brain" )
+        .append( "“Building Better Brains: Development, Mental Health, and Addiction” a video from the Alberta Family Wellness Initiative" );
+   
+    $( ".caption-exec-func" )
+        .append( "“Executive Function: Skills for Life and Learning,” a video from the Center on the Developing Child at Harvard University" );
+    
+    $( ".caption-dev-strength" )
+        .append( "“Serve and Return,” a video from Alberta Family Wellness" );
+    
+    $( ".caption-mcgill" )
+        .append( "Moshe Szyf, a molecular biologist at McGill University, discusses the way parental licking and grooming affects the expression of certain genes in rat pups." );
+    
+    $( ".caption-felitti" )
+        .append( "Dr. Vincent Felitti Discusses the Genesis and Findings of the Adverse Childhood Experiences Study" );
+    
+    $( ".caption-prob-adults" )
+        .append( "The More Categories of Trauma Children Experience, The More Problems They Face as Adults" );
+    
+    $( ".caption-adv-env" )
+        .append( "“How Childhood Trauma Affects Health Across A Lifetime,” a TED Talk by Nadine Burke Harris." );
+    
+    $( ".caption-ace-scores" )
+        .append( "Children Who Experience Trauma Often Have Behavior Problems in School" );
+    
+    $( ".caption-continuum" )
+        .append( "“The Science of Neglect,” a video from the Center on the Developing Child at Harvard University" );
+    
+    $( ".caption-brain-power" )
+        .append( "“Brain Power: Why Early Learning Matters,” a talk by Patricia Kuhl, co-director of the University of Washington Institute for Learning and Brain Sciences, at the 2011 NBC News Education Nation Summit" );
+   
+    $( ".caption-effects-env" )
+        .append( "“The Importance of Early Child Development,” a talk by Clyde Hertzman, director of the Human Early Learning Partnership" );
+    
+    $( ".caption-early-child-spending" )
+        .append( "Most of the Public Money Spent on Early Childhood Goes to 3-, 4-, and 5-Year-Olds" );
+    
+    $( ".caption-team-res" )
+        .append( "“Should We Invest in Home Stimulation Programmes?,” an interview with Sally M. Grantham-McGregor" );
+    
+    $( ".caption-jamaica-home" )
+        .append( "tbd Participants in the Jamaica Home-Visiting Study" );
+    
+    $( ".caption-sec-attach" )
+        .append( "Securely Attached Babies Thrive When They Get to Preschool" );
+    
+    $( ".caption-lynchson" )
+        .append( "Mary Dozier (left), the psychology researcher who developed Attachment and Biobehavioral Catch-up, with Amy Lynch and Lynch's son, Asian, who was adopted from Russia. Credit: University of Delaware/Kathy Atkinson." );
+    
+    $( ".caption-mothers-abc" )
+        .append( "When Mothers Take Part in the ABC Home-Visiting Program, Their Children’s Stress Levels Improve" );
+    
+    $( ".caption-caregiver" )
+        .append( "“FIND: Using Science to Coach Caregivers,” a video from the Center on the Developing Child at Harvard University" );
+    
+    $( ".caption-before-five" )
+        .append( "Illustration taken from James  J. Heckman, “The American Family in Black & White: A Post-Racial Strategy for Improving Skills to Promote Equality.” As Heckman writes in that paper, “Gaps in test scores classified by social and economic status of the family emerge at early ages, before schooling starts, and they persist. Similar gaps emerge and persist in indices of soft skills classified by social and economic status. Again, schooling does little to widen or narrow these gaps.” (p. 76)" );
+   
+    $( ".caption-educare-prem" )
+        .append( "“The Power of Relationships,” a video from Educare" );
+    
+    $( ".caption-omaha-ctr" )
+        .append( "Educare Tulsa at Kendall Whitter. Architecture: RDG Planning & Design and KKT Architects" );
+    
+    $( ".caption-exceed-score" )
+        .append( "Children in Educare For At Least Two Years Exceed State Reading-Score Expectations" );
+    
+    $( ".caption-aok-prog" )
+        .append( "All Our Kin Program Video" );
+    
+    $( ".caption-pro-dev" )
+        .append( "CSRP Program Video" );
+    
+    $( ".caption-raver-lect" )
+        .append( "“Programs and Policies Supporting Self-Regulation in the Context of Poverty,” a lecture by Cybele Raver" );
+    
+    $( ".caption-blocks" )
+        .append( "Building Blocks for Learning" );
+   
+    $( ".caption-walla" )
+        .append( "“Resilient: The School Discipline Revolution in Walla Walla, WA,” a video from the League of Education Voters" );
+    
+    $( ".caption-suspended" )
+        .append( "High School Students with Disadvantages Are Suspended More Often than Other Students" );
+    
+    $( ".caption-oos" )
+        .append( "Most Out-of-School Suspensions in Chicago High Schools Are for Non-Violent Behavior" );
+    
+    $( ".caption-roland" )
+        .append( "“Economist Roland Fryer: 2011 MacArthur Fellow,” a video from the MacArthur Foundation" );
+    
+    $( ".caption-roland-pic" )
+        .append( "Roland Fryer" );
+    
+    $( ".caption-deci-ryan" )
+        .append( "Highlights of the Opening Remarks from the 5th Conference on Self-Determination Theory,” a talk by Richard Ryan and Edward Deci" );
+    
+    $( ".caption-deci-ryan-photo" )
+        .append( "University of Rochester Psychology Professors Richard Ryan (left) and Edward Deci" );
+    
+    $( ".caption-dont-work" )
+        .append( "“Measurement Matters: Assessing Personal Qualities Other Than Cognitive Ability for Educational Purposes,” a video of Angela Duckworth and David Yeager" );
+    
+    $( ".caption-jackson" )
+        .append( "“Non-Cognitive Ability, Test Scores, and Teacher Quality: Evidence from 9th Grade Teachers in North Carolina,” a talk by Kirabo Jackson" );
+    
+    $( ".caption-kirabo" )
+        .append( "“Non-Cognitive Ability, Test Scores, and Teacher Quality: Evidence from 9th Grade Teachers in North Carolina,” a talk by Kirabo Jackson" );
+    
+    $( ".caption-farrington" )
+        .append( "Camille Farrington of the Consortium on Chicago School Research at the University of Chicago" );
+    
+    $( ".caption-farrington-ans" )
+        .append( "“Why Motivation and Deeper Learning Matter,” a presentation by Camille Farrington and David Yeager" );
+    
+    $( ".caption-stereo" )
+        .append( "Claude Steele on Stereotype Threat" );
+    
+    $( ".caption-growth-mindset" )
+        .append( "“The Power of Believing that You Can Improve,” a TED talk by Carol Dweck" );
+    
+    $( ".caption-cohen" )
+        .append( "“Closing the Achievement Gap,” a talk by Geoffrey Cohen" );
+    
+    $( ".caption-fback" )
+        .append( "Feedback from Teachers Affects Student Engagement and Performance" );
+    
+    $( ".caption-man" )
+        .append( "Becoming a Man” Group-Mentoring Program Reduces Violent-Crime Arrests and Increases On-Time Graduation" );
+    
+    $( ".caption-rand-trials" )
+        .append( "“Jens Ludwig presents BAM crime results in 2012”" );
+    
+    $( ".caption-bam-hist" )
+        .append( "Anthony Ramirez-DiVittorio, the founder of the Becoming a Man program, discusses BAM’s history." );
+    
+    $( ".caption-turnaround" )
+        .append( "“Partners in School Transformation,” a video from Turnaround for Children" );
+    
+    $( ".caption-cantor" )
+        .append( "Pamela Cantor, the founder of Turnaround for Children, discusses what schools need to do to better serve students living in poverty" );
+    
+    $( ".caption-el-ed" )
+        .append( " “Critique and Feedback - the Story of Austin’s Butterfly,” a class on expeditionary learning taught by EL’s Chief Academic Officer, Ron Berger" );
+    
+    $( ".caption-crew" )
+        .append( "“Crew: A Structure for Support,” a video from EL Education" );
+    
+    $( ".caption-brady" )
+        .append( "Molly Brady, a teacher at Polaris Charter Academy, with some of her students" );
+    
+    $( ".caption-think-pos" )
+        .append( "“More Than You Think Possible,” a video from EL Education" );
+    
+    $( ".caption-wheels" )
+        .append( "President Obama Speaks about WHEELS" );
+    
+    $( ".caption-rep-prac" )
+        .append( "More than 80% of the Average Elementary School Student’s Classroom Time Is Spent on Basic Skills and Not Problem-Solving or Reasoning" );
+    
+    $( ".caption-more-rep-prac" )
+        .append( "Students in Elementary Schools Serving Low-Income Children are Signficantly More Likely to Be Engaged in Repetitive Practice In Class" );
+    
+    $( ".caption-amerimore" )
+        .append( "American Students Spend More Time on Repetitive Practice than Japanese Students" );
+    
+    $( ".caption-stigler" )
+        .append( "“Lessons from Japanese and Chinese Education,” a talk by James Stigler" );
+    
+    $( ".caption-geo" )
+        .append( "Japanese 8th Grade Geometry Class, a video from Stigler’s study" );
+    
+    $( ".caption-skillset" )
+        .append( "The teacher perspective on deeper learning, a video by the Hewlett Foundation" );
+    
+     $( ".caption-edutopia" )
+        .append( "Video about inquiry-based learning from Edutopia" );
+    
+     $( ".caption-proj-based" )
+        .append( "Video about project-based learning from Edutopia" );
+    
+    $( ".caption-perf-based" )
+        .append( "Video about project-based learning from Edutopia" );
+    
+    $( ".caption-gradation" )
+        .append( "Almost Half of Poor American Children Are Living in Deep Poverty" );
+    
+});
+
+
+
+
+
+

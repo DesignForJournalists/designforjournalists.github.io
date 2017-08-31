edbc78229c98edfa7a987af88108236e5872f143
dylbop <dylangreif@Dylans-MacBook-Pro-2.local>
2017-08-30 22:50:06 -0400

    updated website for fall 2017

--- folder/js/script.js
+++
@@ -1,4 +0,0 @@
-$('iframe').load( function() {
-    $('iframe').contents().find("head")
-      .append($("<style type='text/css'>  body{width:100% !important;max-width:100% !important;}  </style>"));
-});
